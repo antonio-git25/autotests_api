977cb63e4a6b435a77da22e41bec5140cdf2baf8
antonio-git25 <antonio-git25@users.noreply.github.com>
2025-12-20 21:06:49 +0000

    deploy: 08831e9d9d3bebcef9657015d92226f1ec741bbd

--- conftest.py
+++
@@ -1,8 +0,0 @@
-pytest_plugins = (
-    "fixtures.fixt_users",
-    "fixtures.fixt_authentication",
-    "fixtures.fixt_files",
-    "fixtures.fixt_courses",
-    "fixtures.fixt_exercises",
-    "fixtures.fixt_allure"
-)
--- fixtures/fixt_allure.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from tools.allure.environment import create_allure_env_file
-
-
-@pytest.fixture(scope='session', autouse=True)
-def save_allure_env_file():
-
-    yield
-    create_allure_env_file()
